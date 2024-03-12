7326e588094c1bfa88b4fefa6d717335f3b5bc06
TheWhatis <anton-gogo@mail.ru>
2024-03-13 03:57:17 +0700

    Добавил класс и методы для работы с остатками

---
+++ src/V1/Stocks.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Файл с классом-сервисом для
+ * работы с остатками
+ *
+ * PHP version 8
+ *
+ * @category V1
+ * @package  OzonSeller
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+
+namespace Whatis\OzonSeller\V1;
+
+use Whatis\OzonSeller\Service\BaseService;
+use Whatis\OzonSeller\Attribute\Mapping;
+
+/**
+ * Класс-сервис для работы
+ * с остатками
+ *
+ * PHP version 8
+ *
+ * @category V1
+ * @package  OzonSeller
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+class Stocks extends BaseService
+{
+    /**
+     * Основной путь до методов
+     *
+     * @var string
+     */
+    public string $path = 'v1/product';
+
+    /**
+     * Обновить остатки
+     *
+     * `v1/product/import/stocks`
+     *
+     * @param array $stocks Остатки товаров
+     *
+     * @return mixed
+     */
+    #[Mapping(':path/import/stocks')]
+    public function update(array $stocks): mixed
+    {
+        return $this->request('POST', "{$this->path}/import/stocks", [
+            'stocks' => $stocks
+        ]);
+    }
+
+    /**
+     * Информация об остатках на складах продавца
+     *
+     * `v1/product/info/stocks-by-warehouse/fbs`
+     *
+     * @param array $skus SKU товаров
+     *
+     * @return mixed
+     */
+    #[Mapping(':path/info/stocks-by-warehouse/fbs')]
+    public function getByWarehouse(array $skus): mixed
+    {
+        return $this->request('POST', "{$this->path}/stocks-by-warehouse/fbs", [
+            'sku' => $skus
+        ]);
+    }
+}
