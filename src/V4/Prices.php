f88e01f09d88485486985f91ca5f6765f1cb338e
TheWhatis <anton-gogo@mail.ru>
2024-03-13 03:58:41 +0700

    Добавил класс и методы для работы с ценами для 4 версии api

---
+++ src/V4/Prices.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Файл с классом-сервисом для
+ * работы с ценами
+ *
+ * PHP version 8
+ *
+ * @category V4
+ * @package  OzonSeller
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+
+namespace Whatis\OzonSeller\V4;
+
+use Whatis\OzonSeller\Service\BaseService;
+use Whatis\OzonSeller\Attribute\Mapping;
+
+/**
+ * Класс-сервис для работы
+ * с ценами
+ *
+ * PHP version 8
+ *
+ * @category V4
+ * @package  OzonSeller
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+class Prices extends BaseService
+{
+    /**
+     * Получить информацию о цене товаров
+     *
+     * `v4/product/info/prices`
+     *
+     * @param int    $limit  Количество выдаваемых значений
+     * @param string $lastId Элемент пагинации
+     * @param array  $filter Фильтры по ценам
+     *
+     * @return mixed
+     */
+    #[Mapping('v4/product/info/prices')]
+    public function get(int $limit, string $lastId = '', array $filter = [])
+    {
+        return $this->request('POST', 'v4/product/info/prices', [
+            'limit' => $limit,
+            'last_id' => $lastId,
+            'filter' => $filter
+        ]);
+    }
+}
