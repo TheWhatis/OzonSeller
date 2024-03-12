77652b6ca46707540065b761486ca6987fba7678
TheWhatis <anton-gogo@mail.ru>
2024-03-13 03:58:29 +0700

    Добавил класс и методы для работы со складами

---
+++ src/V1/Warehouse.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Файл с классом-сервисом для
+ * работы со складами
+ *
+ * PHP version 8
+ *
+ * @category V1
+ * @package  OzonSeller
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+
+namespace Whatis\OzonSeller\V1;
+
+use Whatis\OzonSeller\Service\BaseService;
+use Whatis\OzonSeller\Attribute\Mapping;
+
+/**
+ * Класс-сервис для работы
+ * со складами
+ *
+ * PHP version 8
+ *
+ * @category V1
+ * @package  OzonSeller
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+class Warehouse extends BaseService
+{
+    /**
+     * Основной путь до методов
+     *
+     * @var string
+     */
+    public string $path = 'v1/warehouse';
+
+    /**
+     * Список складов
+     *
+     * `v1/warehouse/list`
+     *
+     * @return mixed
+     */
+    #[Mapping(':path/list')]
+    public function list(): mixed
+    {
+        return $this->request('POST', "{$this->path}/list");
+    }
+
+    /**
+     * Список методов доставки склада
+     *
+     * `v1/warehouse/delivery-method/list`
+     *
+     * @param int   $limit  Количество возвращаемых методов
+     * @param int   $offset Отступ (элемент пагинации)
+     * @param array $filter Фильтры
+     *
+     * @return mixed
+     */
+    #[Mapping(':path/delivery-method/list')]
+    public function deliveryMethods(int $limit, int $offset = 0, $filter = []): mixed
+    {
+        return $this->request('POST', "{$this->path}/delivery-method/list", [
+            'limit' => $limit,
+            'offset' => $offset,
+            'filter' => $filter
+        ]);
+    }
+}
